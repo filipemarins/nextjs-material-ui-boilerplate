7c0062f9d8edbc876ae861b9cd4db57152f7ce65
filipemarins <lipe_bm@yahoo.com.br>
2020-02-07 01:06:24 -0300

    Add themes folder

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,10 @@ import App from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../src/theme';
+import theme from '../src/themes/dark';
 
 export default class MyApp extends App {
   componentDidMount() {
-    // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
@@ -24,7 +23,6 @@ export default class MyApp extends App {
           <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         </Head>
         <ThemeProvider theme={theme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
           <CssBaseline />
           <Component {...pageProps} />
         </ThemeProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core/styles';
-import theme from '../src/theme';
+import theme from '../src/themes/dark';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <html lang="en">
         <Head>
-          {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link
             rel="stylesheet"
